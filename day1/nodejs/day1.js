06c2684057d0e3a64b5181a6ab8335f108b5d70d
cumuli33 <andreaslutzenberger@googlemail.com>
2018-12-06 16:43:45 +0100

    [DAY 1]refactored part 1 and added tests part2

--- day1/nodejs/day1.js
+++ day1/nodejs/day1.js
@@ -1,9 +1,16 @@
 const calculateFrequency = (input) => {
+    const operations = parseInputToOperations(input);    
+    return executeOperations(operations);
+}
+
+const parseInputToOperations = (input) =>  {
     const regex = /([+-]?)(\d*)/;
-    operations = input.split('\n').map(value => regex.exec(value)).map(value => ({ operator: value[1], value: Number(value[2])}));
-    
-    var result = 0;
-    
+    return input.split('\n')
+            .map(value => regex.exec(value))
+            .map(value => ({ operator: value[1], value: Number(value[2])}));
+}
+
+const executeOperations = (operations, result = 0) => {
     operations.forEach(operation => {
         switch(operation.operator) {
             case '+':
@@ -13,9 +20,13 @@ const calculateFrequency = (input) => {
                 result -= operation.value;
                 break;
         }
-    })
-    
+    });
     return result;
 }
 
+const findRepeatingFrequency = (input) => {
+    return 0;
+}
+
+exports.findRepeatingFrequency = findRepeatingFrequency;
 exports.calculateFrequency = calculateFrequency;
--- day1/nodejs/test/test.js
+++ day1/nodejs/test/test.js
@@ -2,24 +2,50 @@ var assert = require('assert');
 var fs = require('fs');
 var day1 = require('../day1');
 describe('Day 1: Chronal Calibration', () => {
-  describe('Examples', () => {
-    it('First Example', () => {
-        const input = fs.readFileSync('./../example1.txt', 'utf8');
-        assert.equal(day1.calculateFrequency(input),3);
-    });
-    it('Second Example', () => {
-        const input = fs.readFileSync('./../example2.txt', 'utf8');
-        assert.equal(day1.calculateFrequency(input),0);
-    });
-    it('Third Example', () => {
-        const input = fs.readFileSync('./../example3.txt', 'utf8');
-        assert.equal(day1.calculateFrequency(input),-6);
+  describe('Part 1', () => {
+    describe('Examples', () => {
+      it('First Example', () => {
+          const input = fs.readFileSync('./../examplep1-1.txt', 'utf8');
+          assert.equal(day1.calculateFrequency(input),3);
+      });
+      it('Second Example', () => {
+          const input = fs.readFileSync('./../examplep1-2.txt', 'utf8');
+          assert.equal(day1.calculateFrequency(input),0);
+      });
+      it('Third Example', () => {
+          const input = fs.readFileSync('./../examplep1-3.txt', 'utf8');
+          assert.equal(day1.calculateFrequency(input),-6);
+      });
+      it('Puzzle Input', () => {
+        const input = fs.readFileSync('./../day1.txt', 'utf8');
+        const result = day1.calculateFrequency(input);
+        assert.notEqual(Number(result),NaN);
+        console.log('Result of Day 1 Part 1: ' + result)
+      });
     });
   });
-  it('Day1 Puzzle', () => {
-    const input = fs.readFileSync('./../day1.txt', 'utf8');
-    const result = day1.calculateFrequency(input);
-    assert.notEqual(Number(result),NaN);
-    console.log('Result of Day 1: ' + result)
+  describe('Part 2', () => {
+    it('First Example', () => {
+        const input = fs.readFileSync('./../examplep2-1.txt', 'utf8');
+        assert.equal(day1.findRepeatingFrequency(input),0);
+    });
+    it('Second Example', () => {
+        const input = fs.readFileSync('./../examplep2-2.txt', 'utf8');
+        assert.equal(day1.findRepeatingFrequency(input),10);
+    });
+    it('Third Example', () => {
+        const input = fs.readFileSync('./../examplep2-3.txt', 'utf8');
+        assert.equal(day1.findRepeatingFrequency(input),5);
+    });
+    it('Fourth Example', () => {
+      const input = fs.readFileSync('./../examplep2-4.txt', 'utf8');
+      assert.equal(day1.findRepeatingFrequency(input),14);
+  });
+    it('Puzzle Input', () => {
+      const input = fs.readFileSync('./../day1.txt', 'utf8');
+      const result = day1.findRepeatingFrequency(input);
+      assert.notEqual(Number(result),NaN);
+      console.log('Result of Day 1 Part 2: ' + result)
+    });
   });
 });
