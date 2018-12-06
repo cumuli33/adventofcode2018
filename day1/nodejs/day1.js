b904d1cb9badd39a9859a64a976c30eb9617c193
cumuli33 <andreaslutzenberger@googlemail.com>
2018-12-06 15:51:57 +0100

    javascript solution for day1

--- day1/nodejs/day1.js
+++ day1/nodejs/day1.js
@@ -1,5 +1,21 @@
 const calculateFrequency = (input) => {
-
+    const regex = /([+-]?)(\d*)/;
+    operations = input.split('\n').map(value => regex.exec(value)).map(value => ({ operator: value[1], value: Number(value[2])}));
+    
+    var result = 0;
+    
+    operations.forEach(operation => {
+        switch(operation.operator) {
+            case '+':
+                result += operation.value;
+                break;
+            case '-':
+                result -= operation.value;
+                break;
+        }
+    })
+    
+    return result;
 }
 
 exports.calculateFrequency = calculateFrequency;
--- day1/nodejs/test/test.js
+++ day1/nodejs/test/test.js
@@ -4,16 +4,22 @@ var day1 = require('../day1');
 describe('Day 1: Chronal Calibration', () => {
   describe('Examples', () => {
     it('First Example', () => {
-        const input = fs.readFileSync('./test/example1.txt', 'utf8');
+        const input = fs.readFileSync('./../example1.txt', 'utf8');
         assert.equal(day1.calculateFrequency(input),3);
     });
     it('Second Example', () => {
-        const input = fs.readFileSync('./test/example2.txt', 'utf8');
+        const input = fs.readFileSync('./../example2.txt', 'utf8');
         assert.equal(day1.calculateFrequency(input),0);
     });
     it('Third Example', () => {
-        const input = fs.readFileSync('./test/example3.txt', 'utf8');
+        const input = fs.readFileSync('./../example3.txt', 'utf8');
         assert.equal(day1.calculateFrequency(input),-6);
     });
+  });
+  it('Day1 Puzzle', () => {
+    const input = fs.readFileSync('./../day1.txt', 'utf8');
+    const result = day1.calculateFrequency(input);
+    assert.notEqual(Number(result),NaN);
+    console.log('Result of Day 1: ' + result)
   });
 });
